747b3923061f625b6607a4589750e1ad60a080d4
Tristan Isham <tristan@isham.co>
2024-08-28 16:58:13 -0400

    added serialization to mouse.hpp

---
+++ src/objects/mouse.cpp
@@ -0,0 +1,26 @@
+#include "mouse.hpp"
+
+
+json cheese::Mouse::TimePoint::serialize() {
+	json j = {
+		{"t", this->timestamp},
+		{"x", this->coords.x},
+		{"y", this->coords.y},
+	};
+
+	return j;
+}
+
+json cheese::Mouse::serialize() {
+	json j{};
+	for (TimePoint& tp : this->buffer) {
+		j.push_back(tp.serialize());
+	}
+
+	return j;
+}
+
+void cheese::Mouse::save() {
+	auto tp = TimePoint{ this->pos() };
+	this->buffer.push_back(tp);
+}
